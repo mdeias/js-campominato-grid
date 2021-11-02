65175b92e2b5f9351f502c8faf67a855729001b0
mdeias <mdeias@icloud.com>
2021-11-02 21:49:17 +0100

    recupero variabili select

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
 
 const container = document.querySelector('.container');
 const listNumbers = [];
-init(100);
+
+
+// variabili select 
+const difficolta = document.getElementById("opzioni");
+console.log(difficolta);
+
+const facile = document.querySelector(".facile");
+console.log(facile);
+
+const normale = document.querySelector(".normale");
+console.log(normale);
+
+const difficile = document.querySelector(".difficile");
+console.log(difficile);
+
+
+let livello = difficolta[normale] ;
+init(livello);  
+
+
+
 
 // Creo il ciclo che genera gli square
 function init(tot){
@@ -9,21 +29,27 @@ function init(tot){
         const sq = createSquare(container);
         console.log(sq);
     }
-    
 }
 
 
 //creo la funzione che genera le caselle
-function createSquare(target) {
 
+function createSquare(target) {
     const sq = document.createElement("div");
     const numbRandom = randomUnique(listNumbers, 1, 100);
     console.log(numbRandom);
     sq.innerHTML = numbRandom ;
     sq.classList.add("square");
-    sq.classList.add("easy");
+    if (livello === 100) {
+        sq.classList.add("easy");
+    }else if (livello === 81) {
+        sq.classList.add("hard");
+    }else{
+        sq.classList.add("crazy");
+    }
     target.append(sq);
     return sq;
+    
 }
 
 
