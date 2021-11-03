f0663c0d16e6abe712105a9271395f8001ef9823
mdeias <mdeias@icloud.com>
2021-11-03 12:25:35 +0100

    nuovo js con procedimento differente

--- js/script.js
+++ js/script.js
@@ -1,80 +1,20 @@
+// recupero il bottone e aggiungo l'evento click
 
-const container = document.querySelector('.container');
-const listNumbers = [];
+document.getElementById("play").addEventListener("click",function(){
+    play();
+})
 
+// creo una funzione che conterrà tutto il procedimento
 
-// variabili select 
-const difficolta = document.getElementById("opzioni");
-console.log(difficolta);
-
-const facile = document.querySelector(".facile");
-console.log(facile);
-
-const normale = document.querySelector(".normale");
-console.log(normale);
-
-const difficile = document.querySelector(".difficile");
-console.log(difficile);
-
-
-let livello = difficolta[normale] ;
-init(livello);  
-
-
-
-
-// Creo il ciclo che genera gli square
-function init(tot){
-    for(let i = 0; i < tot; i++){
-        const sq = createSquare(container);
-        console.log(sq);
+function play(){
+    const opzioni = 
+   parseInt(document.getElementById("opzioni").value);
+    let numeroCelle = 100;
+    if (opzioni === 2) {
+        numeroCelle = 81;
+    }else if (opzioni === 3) {
+        numeroCelle = 49;
     }
+    console.log(numeroCelle);
 }
-
-
-//creo la funzione che genera le caselle
-
-function createSquare(target) {
-    const sq = document.createElement("div");
-    const numbRandom = randomUnique(listNumbers, 1, 100);
-    console.log(numbRandom);
-    sq.innerHTML = numbRandom ;
-    sq.classList.add("square");
-    if (livello === 100) {
-        sq.classList.add("easy");
-    }else if (livello === 81) {
-        sq.classList.add("hard");
-    }else{
-        sq.classList.add("crazy");
-    }
-    target.append(sq);
-    return sq;
-    
-}
-
-
-// rendo unici i numeri random
-function randomUnique(list, min, max) {
-    let number = null;
-    let valid = false;
-
-    while (!valid) {
-        number = randomNumbgenerator(min, max);
-        if (!list.includes(number)) {
-            valid = true;
-            list.push(number);
-        }
-    }
-    return number;
-}
-
-
-// creo generatore numeri random con una funzione
-function randomNumbgenerator(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-
-
-
 
