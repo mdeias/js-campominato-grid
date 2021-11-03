fafe422ce0aa858db2e6486211f6513c4e005227
mdeias <mdeias@icloud.com>
2021-11-03 13:26:50 +0100

    funzione che genera celle e contenitore

--- js/script.js
+++ js/script.js
@@ -16,5 +16,35 @@ function play(){
         numeroCelle = 49;
     }
     console.log(numeroCelle);
+    //resetto il main
+    document.querySelector("main").innerHTML = "";
+    // chiamo la funzione che genererà la griglia e le celle
+    generatoreCella();
+
+    // creo le celle e il loro container
+   function generatoreCella() {
+       const container = document.createElement("div");
+       container.classList.add("container");
+       console.log(container);
+       for (let i = 1; i <= numeroCelle; i++) {
+          const cella = document.createElement("div");
+          cella.className = "cella";
+          cella.innerHTML = i;
+          if (numeroCelle === 100) {
+              cella.classList.add("easy");
+          }else if (numeroCelle === 81) {
+              cella.classList.add("hard");
+          }else{
+              cella.classList.add("crazy");
+          }
+          console.log(cella);
+          container.append(cella);
+        }
+        document.querySelector("main").append(container);
+   }
+
 }
+
+
+
 
